56ccf286f8ae898cdfec555eb49b901d4fc775f3
Jatin Goyal <25JG2001@GMAIL.COM>
2022-07-02 23:46:31 +0530

    home, sidebar , navbar

---
+++ letsshare_frontend/src/components/Feed.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { client } from "../client";
+
+const Feed = () => {
+  return <div>Feed</div>;
+};
+
+export default Feed;
